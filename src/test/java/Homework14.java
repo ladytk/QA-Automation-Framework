9869569df1bf6f1a481536d7a99aac7cbfcd1e6b
ladytk <80181608+ladytk@users.noreply.github.com>
2022-10-15 16:11:44 -0400

    Homework14
    Part Deux

--- src/test/java/Homework14.java
+++ src/test/java/Homework14.java
@@ -31,8 +31,25 @@ public class Homework14 {
             Thread.sleep(5000);
             driver.quit();
             }
-        }
 
+    @Test
+    public static void NavigateToRegistration () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement registrationButton = driver.findElement(By.xpath("//a[@href='/registration.php']"));
+        //WebElement registrationButton = driver.findElement(By.linkText("https://bbb.testpro.io/registration.php"));
+        registrationButton.click();
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+        }
 
 //    Using Selenium, Navigate to "https://bbb.testpro.io/"
 //        Click Registration link
